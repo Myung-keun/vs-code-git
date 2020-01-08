c55cecf2b98e96669dd1b076b75845dac2800b5d
Sung Ji Eun <jseong@energyx.co.kr>
2020-01-08 11:40:42 +0900

    js 수정

--- jeSeong/launch menu/js/menu1.js
+++ jeSeong/launch menu/js/menu1.js
@@ -4,18 +4,18 @@ function addRow(){
     var row = my_tbody.insertRow(my_tbody.rows.length);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
-    var cell2 = row.insertCell(2);
-    var cell2 = row.insertCell(3);
-    var cell2 = row.insertCell(4);
-    var cell2 = row.insertCell(5);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
 
 }
 
 function delRow(){
     var my_tbody = document.getElementById('my_tbody');
     if (my_tbody.rows.length < 1) return;
-    my_tbody.deleteRow(0);
-    my_tbody.delRow(my_tbody.rows.length -1);
+    //my_tbody.deleteRow(0);//
+    my_tbody.deleteRow(my_tbody.rows.length-1);
 }
 
 
@@ -43,4 +43,3 @@ function HighLightRow(obj){
 
 
 //local storage에 저장하고 다시 배열해주는 함수
-function 
