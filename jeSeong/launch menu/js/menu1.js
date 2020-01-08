5b2f93f28aae0235fc5d69bc61c39ba399412589
Sung Ji Eun <jseong@energyx.co.kr>
2020-01-08 11:20:45 +0900

    js 칸 추가하고 삭제하는 함수 추가

--- jeSeong/launch menu/js/menu1.js
+++ jeSeong/launch menu/js/menu1.js
@@ -1,11 +1,46 @@
+//행 추가와 삭제하는 함수
 function addRow(){
-    mytable=document.getElementsById('stock_form');
-    row = mytable.insertRow(mytable.rows.length);
+    var my_tbody = document.getElementById('my_tbody');
+    var row = my_tbody.insertRow(my_tbody.rows.length);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    var cell2 = row.insertCell(3);
+    var cell2 = row.insertCell(4);
+    var cell2 = row.insertCell(5);
 
-    cell1=row.insertCell(6);
-    cell2=row.insertCell(1);
-
-    cell1.innerHTML = "<td class='al fontB'><input type='text' name='title' size='10'></td>"
-    cell2.innerHTML = "<td><input type='text' name='title' size='10'></td>"
 }
 
+function delRow(){
+    var my_tbody = document.getElementById('my_tbody');
+    if (my_tbody.rows.length < 1) return;
+    my_tbody.deleteRow(0);
+    my_tbody.delRow(my_tbody.rows.length -1);
+}
+
+
+
+
+//테이블 칸 클릭되게하는 함수
+function HighLightRow(obj){
+    var table = document.getElementsByClassName(".table1");
+    var tr = table.getElementsByTagName("tr");
+    for(var i =0; i<tr.length; i++){
+        tr[i].style.background = "pink";
+    }
+    obj.style.backgroundcolor = "pink";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//local storage에 저장하고 다시 배열해주는 함수
+function 
