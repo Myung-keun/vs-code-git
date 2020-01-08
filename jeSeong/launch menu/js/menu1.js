6158245f18c4a375c45500956a7d266acbd3bc0d
Sung Ji Eun <jseong@energyx.co.kr>
2020-01-08 10:49:37 +0900

    js 수정

--- jeSeong/launch menu/js/menu1.js
+++ jeSeong/launch menu/js/menu1.js
@@ -1,5 +1,11 @@
-function popup( ) {
-    console.log('fff');
+function addRow(){
+    mytable=document.getElementsById('stock_form');
+    row = mytable.insertRow(mytable.rows.length);
+
+    cell1=row.insertCell(6);
+    cell2=row.insertCell(1);
+
+    cell1.innerHTML = "<td class='al fontB'><input type='text' name='title' size='10'></td>"
+    cell2.innerHTML = "<td><input type='text' name='title' size='10'></td>"
 }
 
-popup();
