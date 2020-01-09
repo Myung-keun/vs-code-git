6e7d5b1b293d4ab84cb8e0db75c0b6719657045f
950416smk <950416song@daum.net>
2020-01-09 11:41:45 +0900

    ADD버튼에 ul추가 이벤트를 삽입 -> 추가된 ul에 li정보를 추후 삽입할 예정

--- mgSong/click.js
+++ mgSong/click.js
@@ -6,6 +6,8 @@ document.querySelectorAll(".listBody ul").forEach(function(ul){
 });
 
 const select = document.querySelector("#selectBtn");
+const add = document.querySelector("#addBtn");
+const insert = document.querySelector(".insert");
 
 //select버튼에 click이벤트시 작동할 함수.
 function init(){
@@ -14,13 +16,11 @@ function init(){
         console.log("click");
         //선택된 항목이 있는 경우 선택된 항목 반영.
         if(document.querySelectorAll("ul.selected").length > 0){
-            temp = document.querySelectorAll("ul.selected");
-            console.log(temp);
+            temp = document.querySelectorAll("ul.selected");          
         } 
         //선택된 항목이 없는 경우 전체 항목이 반영
         else{
             temp = document.querySelectorAll(".listBody ul");
-            console.log(temp);
         }
         let count = temp.length;
         let random = Math.floor(Math.random()*count);
@@ -32,4 +32,16 @@ function init(){
     //랜덤숫자 번째 선택된 항목의 첫번째 li항목의 내용(식당이름) 반환.
 }
 
+function addEvent(){
+    const ul = document.createElement("ul");
+    ul.className = "inserted";
+    insert.appendChild(ul);
+    console.log("addBtn is clicked");
+}
+
+function clickAdd(){
+    add.addEventListener("click",addEvent);
+}
+
 init();
+clickAdd();
