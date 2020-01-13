e413bbb8235bf7e3462facaa3bd370c4024a8f3a
950416smk <950416song@daum.net>
2020-01-13 17:54:23 +0900

    addStorage의 기능 구상

--- Restaurant/LStorage.js
+++
@@ -1,8 +0,0 @@
-/* 현재 페이지에 구현사항
-1)현재 LocalStorage에 존재하는 value(이름/위치/메뉴)를
-새로운 tr을 생성 후 td*3, 각각의 td에 넣고 출력해줌.
-
-2)ADD버튼을 클릭했을 때, LocalStorage에 key(restaurant):value(이름/위치/메뉴)
-를 저장 후 새로운 행 생성 && 출력
-
-3)*/
