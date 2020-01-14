57339d0ee54c8a8594f79f9175463b0af048cddc
950416smk <950416song@daum.net>
2020-01-14 17:12:16 +0900

    button css

--- Restaurant/ranSelect.js
+++ Restaurant/ranSelect.js
@@ -1,15 +1,19 @@
-/* tr을 클릭하여 선택 또는 해제 && 선택된 tr에서 랜덤으로 식당의 정보를 alert로 보여주는 함수*/
+/* tr을 클릭하여 선택 또는 해제 && 선택된 tr에서 랜덤으로 식당의 정보를 alert로 보여주는 함수제작 예정*/
 document.querySelectorAll("tbody tr").forEach(function(tr){
     tr.addEventListener("click", function(){
         tr.className = tr.className == "" ? "selected" : "";
     });
 });
 
+
+
 //X버튼 클릭시 선택된 tr을 삭제하는 함수 제작 예정.
 const deleteBtn = document.querySelector("#delete");
 deleteBtn.addEventListener("click", deleteRes);
 
-
+const tBody = document.querySelector("tbody");
+const selectedTr = document.querySelector(".selected");
 function deleteRes(){
+    tBody.removeChild(selectedTr);
     console.log("delete is complete");
 }
