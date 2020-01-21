3ea9fc5b43b57ba9794b7db8d7a1f7aba1588771
Sung Ji Eun <jseong@energyx.co.kr>
2020-01-21 16:06:01 +0900

    페이징 함수2

--- Restaurant/RestaurantTable.js
+++ Restaurant/RestaurantTable.js
@@ -120,12 +120,48 @@ function makeResObj(name, loca, distance ,menu){
 
 
 //페이징 하는 함수 2
-function setPage(listCount, currentPage){
-    var listCount = listCount; //전체 게시글 수
-    var pageCount = (parseInt (listCount/19) + 1); //페이지 개수
-    var currentPage = currentPage; //현재 페이지
-    var endPage = (parseInt(pageCount/19 +1)*5) + 1; //최종 페이지
-    var displayPage = parseInt((currentPage + 4)/5)*5; //밑에 보여줄 페이지
-    
+function setPage(listCount, currentPage, kwd) {
+    var kwd = kwd; // 검색어
+    var listCount = listCount;  // 전체 게시글 수
+    var pageCount = (parseInt( listCount/ 10) + 1); // 페이지 개수
+    var currentPage = currentPage; // 현재 페이지
+    var endPage = (parseInt(pageCount/10 + 1) * 5)+1; // 최종 페이지
+    var displayPage = parseInt((currentPage + 4 ) / 5 ) * 5; // 밑에 보여줄 페이지
+
+    /* 게시글 수가 페이지 수와 딱 맞을 땐 다음 페이지 안보이게*/
+    if(parseInt( listCount% 10)==0){
+        pageCount -=1;
+    };
+    console.log("listCount", listCount);
+    console.log("pageCount", pageCount);
+    console.log("currentPage", currentPage);
+    console.log("endPage", endPage);
+    console.log("displayPage", displayPage);
+
+    var pager = $('#page');
+        if(currentPage <= 5){
+        pager.prepend('<tr>◀</tr>');
+    }else{
+        pager.append('<tr><a href=/Restaurant/Restaurant/'+(displayPage-5)+'?kwd='+kwd+'>'+'◀'+'</tr>');
+    }
+
+    for (var i = displayPage-4; i <= displayPage; i++) {
+        if(i==currentPage){
+            pager.append('<tr class="selected">'+i+'</tr>')
+            continue;
+        }else if(i>pageCount){
+            pager.append('<tr>'+i+'</tr>')
+            continue;
+        }
+        pager.append('<tr><a href=/Restaurant/Restaurant/'+i+'?kwd='+kwd+'>'+i+'</tr>');
+    }
+
+    var nextPage = displayPage+1
+
+    if(currentPage < endPage && endPage < pageCount){
+        pager.append('<tr><a href=/Restaurant/Restaurant/'+nextPage+ '?kwd='+kwd+'>'+'▶'+'</tr>');
+    }else{
+        pager.append('<tr>'+'▶'+'</tr>');
+}
 
 }
